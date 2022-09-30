6da9fe0e39876d13794ead2505594a1f0029cf79
Gonzalo Moyano <gmoyanodvlpr@gmail.com>
2022-09-29 21:03:13 -0300

    Inicialización Proyecto React. Component Navbar

---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,15 @@
+import './Navbar.css'
+
+
+const Navbar = () => {
+    return (
+        <nav>
+            <h1>Hardfire</h1>
+            <div>
+                <button></button>
+            </div>
+        </nav>
+    )
+}
+
+export default Navbar
